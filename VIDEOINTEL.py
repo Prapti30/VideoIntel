927b9ff55c7830e5cc2b94926c90bd3d71af371c
prapti30 <praptimore78@gmail.com>
2025-04-12 14:39:02 +0530

    gg

--- VIDEOINTEL.py
+++ VIDEOINTEL.py
@@ -8,7 +8,7 @@ import msal
 client_id = "cfa7fc3c-0a7c-4a45-aa87-f993ed70fd9e"  # Azure AD Application Client ID
 client_credential = "uAH8Q~RMG~Dy1hRt1dx6IOhtj39j-gmXImKlTaGr"  # Azure AD Application Client Secret
 tenant_id = "94a76bb1-611b-4eb5-aee5-e312381c32cb"  # Azure AD Tenant ID
-redirect_uri = "https://video-intel-cg.streamlit.app/"  # Make sure this matches in Azure
+redirect_uri = "https://video-intel-cg.streamlit.app/test"  # Make sure this matches in Azure
 
 # authorize_url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/authorize"
 # token_url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
